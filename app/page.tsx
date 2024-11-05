d5c6b65a8fdbabe3526db9002f846e133a29f066
Abdullah Arif <160360851+AbdullahArif17@users.noreply.github.com>
2024-11-05 22:51:07 +0500

    Nextjs Presentation

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,327 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 space-y-10">
+      <div className="w-5/6 max-w-3xl text-center bg-white p-10 shadow-lg rounded-lg">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          className="m-8 p-8 mx-auto"
+          src={"/next.svg"}
+          alt={"image"}
+          width={300}
+          height={300}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <h1 className="text-3xl font-bold text-gray-800 mb-6">
+          Exploring Next.js: Key Concepts and Tools
+        </h1>
+        <div className="text-left">
+          <h2 className="text-2xl font-semibold text-blue-600 mb-4">
+            Introduction
+          </h2>
+          <h3 className="text-xl font-medium text-gray-700 mb-4">
+            Welcome to Next.js
+          </h3>
+          <ul className="list-none text-lg text-gray-600 space-y-2">
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Brief introduction to Next.js
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Why it’s popular for building web applications?
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Objective of the presentation: to introduce key concepts like
+              routing, components, and styling
+            </li>
+          </ul>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div className="w-5/6 max-w-3xl text-center bg-white p-10 shadow-lg rounded-lg">
+        <Image
+          className="m-8 p-8 mx-auto"
+          src={"/file.svg"}
+          alt={"image"}
+          width={300}
+          height={300}
+        />
+
+        <div className="text-left">
+          <h2 className="text-2xl font-semibold text-blue-600 mb-4">
+            The page.tsx File
+          </h2>
+          <h3 className="text-xl font-medium text-gray-700 mb-4">
+            What is page.tsx?
+          </h3>
+          <ul className="list-none text-lg text-gray-600 space-y-2">
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Purpose: Holds the main content and structure for a page in
+              Next.js
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Located in the pages directory; each file represents a route
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Automatically routes to / or /[filename]
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-5/6 max-w-3xl text-center bg-white p-10 shadow-lg rounded-lg">
+        <Image
+          className="m-8 p-8 mx-auto"
+          src={"/globe.svg"}
+          alt={"image"}
+          width={300}
+          height={300}
+        />
+
+        <div className="text-left">
+          <h2 className="text-2xl font-semibold text-blue-600 mb-4">
+            The layout.tsx File
+          </h2>
+          <h3 className="text-xl font-medium text-gray-700 mb-4">
+            What is layout.tsx?
+          </h3>
+          <ul className="list-none text-lg text-gray-600 space-y-2">
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Used for wrapping page components in a consistent layout across
+              multiple pages (e.g., headers, footers)
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Reduces code repetition and keeps layouts organized
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Located in the app or pages folder, depending on the Next.js
+              version
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-5/6 max-w-3xl text-center bg-white p-10 shadow-lg rounded-lg">
+        <Image
+          className="m-8 p-8 mx-auto"
+          src={"/window.svg"}
+          alt={"image"}
+          width={300}
+          height={300}
+        />
+
+        <div className="text-left">
+          <h2 className="text-2xl font-semibold text-blue-600 mb-4">
+            The Link Tag
+          </h2>
+          <h3 className="text-xl font-medium text-gray-700 mb-4">
+            What is the Link Tag?
+          </h3>
+          <ul className="list-none text-lg text-gray-600 space-y-2">
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Explanation of Link in Next.js (next/link)
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Purpose: Enables client-side navigation, making page transitions
+              smoother and faster
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Helps improve user experience and SEO by handling internal routing
+              effectively
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-5/6 max-w-3xl text-center bg-white p-10 shadow-lg rounded-lg">
+        <Image
+          className="m-8 p-8 mx-auto bg-black"
+          src={"/vercel.svg"}
+          alt={"image"}
+          width={300}
+          height={300}
+        />
+
+        <div className="text-left">
+          <h2 className="text-2xl font-semibold text-blue-600 mb-4">
+            Components in Next.js
+          </h2>
+          <h3 className="text-xl font-medium text-gray-700 mb-4">
+            What are Components?
+          </h3>
+          <ul className="list-none text-lg text-gray-600 space-y-2">
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Components are reusable building blocks of code
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Purpose: Encourage code modularity, reusability, and scalability
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Breaks down complex UIs into smaller, manageable pieces
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-5/6 max-w-3xl text-center bg-white p-10 shadow-lg rounded-lg">
+        <Image
+          className="m-8 p-8 mx-auto"
+          src={"/file.svg"}
+          alt={"image"}
+          width={300}
+          height={300}
+        />
+
+        <div className="text-left">
+          <h2 className="text-2xl font-semibold text-blue-600 mb-4">
+            Styling in Next.js
+          </h2>
+          <h3 className="text-xl font-medium text-gray-700 mb-4">
+            Styling in Next.js
+          </h3>
+          <ul className="list-none text-lg text-gray-600 space-y-2">
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Next.js supports CSS Modules, Sass, and global CSS
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Applying CSS: Import CSS files directly into components, or use
+              scoped styles with CSS Modules
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Encourages maintaining styles close to components
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-5/6 max-w-3xl text-center bg-white p-10 shadow-lg rounded-lg">
+        <Image
+          className="m-8 p-8 mx-auto"
+          src={"/globe.svg"}
+          alt={"image"}
+          width={300}
+          height={300}
+        />
+
+        <div className="text-left">
+          <h2 className="text-2xl font-semibold text-blue-600 mb-4">
+            Introduction to Tailwind CSS
+          </h2>
+          <h3 className="text-xl font-medium text-gray-700 mb-4">
+            What is Tailwind CSS?
+          </h3>
+          <ul className="list-none text-lg text-gray-600 space-y-2">
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>A
+              utility-first CSS framework that allows rapid UI development
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Uses predefined classes for styling directly in the HTML/JSX,
+              avoiding separate CSS files for smaller styling needs
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Benefits: Highly customizable, reduces CSS bloat, and simplifies
+              styling with a consistent design system
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-5/6 max-w-3xl text-center bg-white p-10 shadow-lg rounded-lg">
+        <Image
+          className="m-8 p-8 mx-auto"
+          src={"/window.svg"}
+          alt={"image"}
+          width={300}
+          height={300}
+        />
+
+        <div className="text-left">
+          <h2 className="text-2xl font-semibold text-blue-600 mb-4">
+            Tailwind CSS vs. Standard CSS
+          </h2>
+          <h3 className="text-xl font-medium text-gray-700 mb-4">
+            Tailwind CSS vs. Standard CSS
+          </h3>
+          <h4 className="text-xl font-medium text-gray-700 mb-4">
+            Tailwind CSS:
+          </h4>
+          <ul className="list-none text-lg text-gray-600 space-y-2">
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Utility classes like flex, bg-color, etc.
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Allows rapid prototyping directly in HTML/JSX
+            </li>
+            <h4 className="text-xl font-medium text-gray-700 mb-4">
+              Standard CSS:
+            </h4>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Uses traditional CSS rules, selectors, and separate files
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              More control over complex animations and specific styling
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-5/6 max-w-3xl text-center bg-white p-10 shadow-lg rounded-lg">
+        <Image
+          className="m-8 p-8 mx-auto bg-black"
+          src={"/vercel.svg"}
+          alt={"image"}
+          width={300}
+          height={300}
+        />
+
+        <div className="text-left">
+          <h2 className="text-2xl font-semibold text-blue-600 mb-4">
+            Summary and Takeaways
+          </h2>
+          <h3 className="text-xl font-medium text-gray-700 mb-4">Summary</h3>
+          <ul className="list-none text-lg text-gray-600 space-y-2">
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Recap main points: purpose of page.tsx and layout.tsx, benefits of
+              Link, importance of components, and styling options
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Emphasize how these features help streamline development in
+              Next.js
+            </li>
+            <li className="flex items-start">
+              <span className="text-blue-600 font-bold mr-2">•</span>
+              Encourage experimenting with Tailwind CSS and components to
+              understand how they work together
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-5/6 max-w-3xl text-center bg-white p-10 shadow-lg rounded-lg">
+        <Image
+          className="m-8 p-8 mx-auto"
+          src={"/next.svg"}
+          alt={"image"}
+          width={300}
+          height={300}
+        />
+      </div>
     </div>
   );
 }
